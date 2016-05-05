38563a55cbf2978f7450e9fc414aec67601a9f26
Xryaznov <xryaznov@gmail.com>
2016-05-05 14:49:05 +0300

    05.05.2016 - 1

--- src/osherove/kata1/StringCalculator.java
+++ src/osherove/kata1/StringCalculator.java
@@ -1,45 +1,43 @@
 package osherove.kata1;
 
-import sun.swing.BakedArrayList;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 /**********************************
  * http://osherove.com/tdd-kata-1 *
  **********************************/
 
 public class StringCalculator {
     public int add(String str) {
-        return str.isEmpty() ? 0 : sum(toIntList(str));
+        return str.isEmpty() ? 0 : sum(toIntArray(str));
     }
 
-    private List<Integer> toIntList(String str) {
-        List<Integer> integers = new ArrayList();
-
-        str = str.replace("\n", ",");
-
-        String[] arr = str.split(",");
-
-        for (String s : arr) {
-            if (!s.isEmpty()) {
-                integers.add(Integer.parseInt(s));
+    private int sum(int[] intArray) {
+        int res = 0;
+        for (int number : intArray) {
+            if (number < 0) {
+                throw new IllegalArgumentException("Negatives are not allowed");
             }
+            res += number;
         }
 
-        return integers;
-    }
-
-    private int sum(List<Integer> integers) {
-
-        int res = 0;
-
-        for (int i : integers) {
-                res += i;
-            }
         return res;
     }
 
+    private int[] toIntArray(String str) {
+        if (str.startsWith("//")) {
+            String delimiter = str.substring(2, 3);
+            str = str.substring(2).replace(delimiter, ",");
+        }
 
+        str = str.replace("\n", ",");
+        String[] strArray = str.split(",");
+
+        int[] intArray = new int[strArray.length];
+
+        for (int i = 0; i < strArray.length; i++) {
+            if (!strArray[i].isEmpty()) {
+                intArray[i] = Integer.parseInt(strArray[i]);
+            }
+        }
+
+        return intArray;
+    }
 }
--- src/osherove/kata1/StringCalculatorTest.java
+++ src/osherove/kata1/StringCalculatorTest.java
@@ -1,29 +1,45 @@
 package osherove.kata1;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import javax.naming.ldap.ExtendedRequest;
-
-import java.util.concurrent.ExecutorService;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class StringCalculatorTest {
+    private StringCalculator calc;
 
-    @Test public void shouldReturnZero() throws Exception {
-        assertEquals(0, new StringCalculator().add(""));
+    @Before
+    public void setUp() {
+        calc = new StringCalculator();
     }
 
-    @Test public void shouldReturnValue() throws Exception {
-        assertEquals(1, new StringCalculator().add("1"));
+    @Test
+    public void shouldReturnZero() {
+        assertEquals(0, calc.add(""));
     }
 
-    @Test public void shouldReturnSum() throws Exception {
-        assertEquals(3, new StringCalculator().add("1,2"));
+    @Test
+    public void shouldReturnInput() {
+        assertEquals(1, calc.add("1"));
     }
 
-    @Test public void shouldHandleNewLines() throws Exception {
-        assertEquals(6, new StringCalculator().add("1\n2,3"));
+    @Test
+    public void shouldReturnSum() {
+        assertEquals(3, calc.add("1,2"));
     }
 
+    @Test
+    public void shouldHandleNewLines() {
+        assertEquals(6, calc.add("1\n2,3"));
+    }
+
+    @Test
+    public void shouldSupportDelimiters() {
+        assertEquals(3, calc.add("//;\n1;2"));
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionOnNegatives() {
+        calc.add("-1");
+    }
 }
