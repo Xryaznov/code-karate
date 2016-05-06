4e854ac6036b65308f2e9f9be68d90c2c4e8f452
Xryaznov <xryaznov@gmail.com>
2016-05-06 23:12:53 +0300

    05.05.2016 - 2

--- src/programmr/challenges/java/QuadrantOfAnAngle.java
+++ src/programmr/challenges/java/QuadrantOfAnAngle.java
@@ -2,13 +2,18 @@ package programmr.challenges.java;
 
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * @see <a href=http://www.programmr.com/challenges/quadrant-angle-1>Quadrant of an Angle</a>
  * <p>
  * A very simple challenge.
  * Write a program which accepts an Integer which is an Angle and prints the Quadrant number in which it is.
+ * <p>
  * For Ex.
  * 1) if user input 30 then it should print 1.
  * 2) if user input 197 then it should print 3.
@@ -17,22 +22,40 @@ import java.util.Scanner;
 public class QuadrantOfAnAngle {
     public static void main(String[] args) {
         int angle;
-        System.out.println("Type an integer angle (in degrees) and press Enter:");
-        Scanner sc = new Scanner(System.in);
-        angle = sc.nextInt();
+
+        if (args.length == 0) {
+            System.out.println("Type an integer angle (in degrees) and press Enter:");
+            Scanner sc = new Scanner(System.in);
+            angle = sc.nextInt();
+        } else {
+            angle = Integer.parseInt(args[0]);
+        }
+
         int quadrant = angle / 90 + 1;
-        System.out.println("Quadrant is:" + quadrant);
+        System.out.println("Quadrant is: " + quadrant);
     }
 
 
     @Test
-    public void test1() {
+    public void shouldPrint1OnAngle30() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
 
+        QuadrantOfAnAngle.main(new String[]{"30"});
+
+        assertTrue(out.toString().contains("Quadrant is: 1"));
+        System.setOut(null);
     }
 
     @Test
-    public void test2() {
+    public void shouldPrint3OnAngle197() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
 
+        QuadrantOfAnAngle.main(new String[]{"197"});
+
+        assertTrue(out.toString().contains("Quadrant is: 3"));
+        System.setOut(null);
     }
 
 }
