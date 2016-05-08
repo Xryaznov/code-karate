da7de403d5d259a81c2685eb5f00f2c4980a8437
Xryaznov <xryaznov@gmail.com>
2016-05-08 21:54:23 +0300

    08.05.2016 - 2

--- src/osherove/kata1/StringCalculator.java
+++ src/osherove/kata1/StringCalculator.java
@@ -1,7 +1,6 @@
 package osherove.kata1;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 /**********************************
  * http://osherove.com/tdd-kata-1 *
@@ -9,36 +8,31 @@ import java.util.List;
 
 public class StringCalculator {
     public int add(String str) {
-        return (str.isEmpty()) ? 0 : sum(str);
+        return str.isEmpty() ? 0 : sum(str);
     }
 
     private int sum(String str) {
         int res = 0;
-        List<Integer> numbers = toIntList(str);
 
-        for (Integer i : numbers) {
-            if (i < 0) {
-                throw new IllegalArgumentException("Negatives are not allowed");
-            }
+        int[] array = toIntArray(str);
+
+        for (int i : array) {
             res += i;
         }
         return res;
     }
 
-    private List<Integer> toIntList(String str) {
+    private int[] toIntArray(String str) {
         if (str.startsWith("//")) {
             String delimiter = str.substring(2, 3);
             str = str.substring(2).replace(delimiter, ",");
         }
-        str = str.replace("\n", ",");
-        String[] stringNumbers = str.split(",");
-        List<Integer> numbers = new ArrayList<>();
 
-        for (String s : stringNumbers) {
-            if (!s.isEmpty()) {
-                numbers.add(Integer.parseInt(s.trim()));
-            }
-        }
-        return numbers;
+        String[] strArray = str.replace("\n", ",").split(",");
+        int[] intArray = Arrays.stream(strArray).filter(s -> ! s.isEmpty())
+                .map(String::trim)
+                .mapToInt(Integer::parseInt).toArray();
+
+        return intArray;
     }
 }
--- src/osherove/kata1/StringCalculatorTest.java
+++ src/osherove/kata1/StringCalculatorTest.java
@@ -1,47 +1,37 @@
 package osherove.kata1;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class StringCalculatorTest {
-    private StringCalculator sc;
 
-    @Before
-    public void setUp() {
-        sc = new StringCalculator();
+    @Test
+    public void shouldReturnZeroOnEmptyString() throws Exception {
+        assertEquals(0, new StringCalculator().add(""));
     }
 
     @Test
-    public void shouldReturnZero() throws Exception {
-        assertEquals(0, sc.add(""));
+    public void shouldReturnSameValueOnOneValue() throws Exception {
+        assertEquals(1, new StringCalculator().add("1"));
     }
 
     @Test
-    public void shouldReturnSameValue() throws Exception {
-        assertEquals(1, sc.add("1"));
+    public void shouldReturnSumOnSeveralValues() throws Exception {
+        assertEquals(3, new StringCalculator().add("1,2"));
     }
 
     @Test
-    public void shouldReturnSum() throws Exception {
-        assertEquals(3, sc.add("1, 2"));
+    public void shouldHandleNewLinesAsDelimiters() throws Exception {
+        assertEquals(6, new StringCalculator().add("1\n2,3"));
     }
 
     @Test
-    public void shouldHandNewLines() {
-        assertEquals(6, sc.add("1\n2, 3"));
+    public void shouldHandleDifferentDelimiters() throws Exception {
+        assertEquals(6, new StringCalculator().add("//;\n1;2;3"));
     }
 
-    @Test
-    public void shouldHandDifferentDelimiters() {
-        assertEquals(6, sc.add("//;\n1;2;3"));
-    }
 
-    @Test (expected = IllegalArgumentException.class)
-    public void shouldFailOnNegatives() {
-        sc.add("-1,2");
-    }
 
 
 }
